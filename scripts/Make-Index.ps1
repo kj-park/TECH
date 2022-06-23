bae198e1f1745ce1639ef8e73298ccd3a6f73e16
Kyoungjoon Park <working@live.co.kr>
2022-06-23 15:22:59 +0900

    .

--- scripts/Make-Index.ps1
+++ scripts/Make-Index.ps1
@@ -1,21 +1,35 @@
 ﻿
 function Make-Index {
     param (
-        [String]$Path,
-        [String]$Output = '.index.md'
+        [String]$Path = 'C:\REPOSITORY\GITHUB\Tech\',
+        [String]$Output = 'index.md'
     )
     begin {
         $contentExts = @('.md')
         $baseUrl = 'https://kj-park.github.io/Tech/<RelativeUrl>'
-        $RootPath = 'C:\REPOSITORY\GITHUB\Tech\'
+        $RootPath = $Path
         $contentItems = $null
-        $MarkdownString = "`n# Contents Index`n`n"
-        $Path = $RootPath
-
+        $MarkdownString = "`n# Contents Index`n"
+        $ContentFolders = @('ActiveDirectory','Exchange','Microsoft365','PowerShell','Learning','includes')
+        $OutputPath = "$RootPath$Output"
     }
     process {
-        if ( Test-Path -LiteralPath $Path ) {
+        if ( Test-Path -LiteralPath $RootPath ) {
+            Set-Content -LiteralPath $OutputPath -Value $MarkdownString -Force
+            
+            foreach ( $itemFolder in $ContentFolders ) {
+                Add-Content -LiteralPath $OutputPath -Value "## [$itemFolder]($itemFolder)`n"
+
+                $Folders = Get-ChildItem -Path "$RootPath$itemFolder" -Directory -Recurse | Where-Object -FilterScript { $PSItem.Name -notcontains @('.media') }
+
+
+
+                Add-Content -LiteralPath $OutputPath -Value "---`n"                
+            }
+            
+            
             $contentItems = Get-ChildItem -Path $Path -Recurse:$true | Where-Object -FilterScript { $PSItem.Extension -in $contentExts }
+
             if ( $contentItems.Count -ne 0 ) {
                 $mdFile = New-Item -Path $Path -Name $Output -ItemType File -Value $MarkdownString -Force
                 foreach ( $item in $contentItems ) {
